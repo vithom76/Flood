f81c96461fb89ff95fc9bd9c43fb097242ddce61
Vitalii <vithom76@gmail.com>
2022-01-18 21:00:25 +0200

    fitst

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+	
+	function hideTabContent() {
+		tabContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active');
+		});
+	} 
+
+	function showTabContent(i = 0) {
+		tabContent[i].classList.add('show', 'fade');
+		tabContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+		
+	}
+
+	hideTabContent();
+	showTabContent();
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((tab, i) => {
+				if (tab == target) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+});

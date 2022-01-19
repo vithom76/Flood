1320c841d9e5906ce2ece50b6dbe6306fbac9ff1
Vitalii <vithom76@gmail.com>
2022-01-19 23:08:21 +0200

    timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+//tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabContent = document.querySelectorAll('.tabcontent'),
 		tabsParent = document.querySelector('.tabheader__items');
+	
 	
 	function hideTabContent() {
 		tabContent.forEach(item => {
@@ -34,4 +36,58 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
-});
+
+	// Timer
+	const deadLine = '2022-01-21';
+
+	function getTimeRemaining(endTime) {
+		const time = Date.parse(endTime) - Date.parse(new Date()),
+			days = Math.floor(time / (1000 * 60 * 60 * 24)),
+			hours = Math.floor(time / (1000 * 60 * 60) % 24),
+			minutes = Math.floor(time / (1000 * 60) % 60),
+			seconds = Math.floor(time / (1000) % 60);
+		
+		return {
+			'total': time,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};		
+	}
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endTime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(apdateClock, 1000);
+		
+		apdateClock();
+		
+		function apdateClock() {
+			const t = getTimeRemaining(endTime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+
+	}
+
+	setClock('.timer', deadLine);
+
+}); 

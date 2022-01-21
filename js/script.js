23bf3195ae066bbe33d92ceca90304a511a422cf
Vitalii <vithom76@gmail.com>
2022-01-21 21:22:04 +0200

    use class

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	// Timer
-	const deadLine = '2022-01-21';
+	const deadLine = '2022-02-21';
 
 	function getTimeRemaining(endTime) {
 		const time = Date.parse(endTime) - Date.parse(new Date()),
@@ -84,6 +84,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			if (t.total <= 0) {
 				clearInterval(timeInterval);
+				days.innerHTML = '';
+			hours.innerHTML = '';
+			minutes.innerHTML = '';
+			seconds.innerHTML = '';
 			}
 		}
 
@@ -140,4 +144,76 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	window.addEventListener('scroll', showmodalByScrol);
+
+	//Используем классы для карточек
+	
+	class CardMenu{
+		constructor(src, alt, title, descr, price, parentSelector, ...classes ){
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUah();
+		}
+		changeToUah(){
+			this.price = this.price * this.transfer;
+		}
+		render() {
+			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => {
+					element.classList.add(className);
+				});
+			}
+			
+				element.innerHTML = `				
+					<img src=${this.src} alt=${this.alt}>
+					<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.descr}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+					</div>				
+				`;
+			this.parent.append(element);		
+			console.log(this.parent);
+		}
+	}
+
+	new CardMenu(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и	высоким качеством!',
+		9,
+		'.menu .container'		
+	).render();
+	
+	new CardMenu(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но икачественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		20,
+		'.menu .container',
+		'menu__item'
+	).render();
+
+	new CardMenu(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие	продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество	белков за счет тофу и импортных вегетарианских стейков.',
+		15,
+		'.menu .container',
+		'menu__item'
+	).render();
 });

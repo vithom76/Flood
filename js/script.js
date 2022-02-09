9c064deec599428dd2e775bdf7457f0ac7b3b0d7
Vitalii <vithom76@gmail.com>
2022-02-09 12:07:04 +0200

    add regexp

--- js/script.js
+++ js/script.js
@@ -372,14 +372,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		dots.push(dot);
 	}
 
+	function deleteNotDigits(str) {
+		return +str.replace(/\D/g, '');
+	}
 
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += deleteNotDigits(width);
 		}
-		console.log(+width.slice(0, width.length - 2));
 		sliderInner.style.transform = `translateX(-${offset}px)`;
 
 		if (slideIndex == slides.length) {
@@ -387,14 +389,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			slideIndex++;
 		}
-		dotsOpacity();
+		currentSlide(slideIndex);
+		dotsOpacity(dots);
 	});
 
-	prev.addEventListener('click', () => {		
+	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset = deleteNotDigits(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= deleteNotDigits(width);
 		}
 		sliderInner.style.transform = `translateX(-${offset}px)`;
 		if (slideIndex == 1) {
@@ -402,29 +405,34 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			slideIndex--;
 		}
-		dotsOpacity();
-		});
+		currentSlide(slideIndex);
+		dotsOpacity(dots);
+	});
 
 	dots.forEach((dot) => {
 		dot.addEventListener('click', (e) => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 			sliderInner.style.transform = `translateX(-${offset}px)`;
-			dotsOpacity();
-		}); 
-		
+			currentSlide(slideIndex);
+			dotsOpacity(dots);
+		});
+
 	});
 
-	function dotsOpacity() {
+	function currentSlide(index) {
 		if (slides.length < 10) {
-			current.textContent = `0${slideIndex}`;
+			current.textContent = `0${index}`;
 		} else {
-			current.textContent = slideIndex;
+			current.textContent = index;
 		}
-		dots.forEach((dot) => {
-			dot.style.opacity = '.5';
+	}
+
+	function dotsOpacity(arr) {
+		arr.forEach((elem) => {
+			elem.style.opacity = '.5';
 		});
-		dots[slideIndex-1].style.opacity = '1';
+		dots[slideIndex - 1].style.opacity = '1';
 	}
 });
